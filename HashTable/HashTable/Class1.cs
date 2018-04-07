28f24bd996ffa172374bbeba7f477e68da914d06
Пользователь <kazancevaaa33@mail.ru>
2018-04-07 12:12:26 +0500

    HT

---
+++ HashTable/HashTable/Class1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTable
+{
+    public class HashTable
+
+    {
+        class KeyValuePair
+        {
+            public object Key { get; set; }
+            public object Value { get; set; }
+        }
+
+        List<List<KeyValuePair>> list;
+        /// <summary>
+        /// Конструктор контейнера
+        /// summary>
+        /// size">Размер хэ-таблицы
+        public HashTable(int size)
+        {
+            list = new List<List<KeyValuePair>>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(new List<KeyValuePair>());
+            }
+        }
+        /// 
+        /// Метод складывающий пару ключь-значение в таблицу
+        /// 
+        /// key">
+        /// value">
+        public void PutPair(object key, object value)
+        {
+            var bucketNo = GetBucketNumber(key);
+            foreach (var kvp in list[bucketNo])
+            {
+                if (Equals(kvp.Key, key))
+                {
+                    kvp.Value = value;
+                    return;
+                }
+            }
+
+            list[bucketNo].Add(new KeyValuePair { Key = key, Value = value });
+        }
+        /// <summary>
+        /// Поиск значения по ключу
+        /// summary>
+        /// key">Ключь
+        /// <returns>Значение, null если ключ отсутствуетreturns>
+        public object GetValueByKey(object key)
+        {
+            var BucketNo = GetBucketNumber(key);
+            foreach (var kvp in list[BucketNo])
+            {
+                if (Equals(kvp.Key, key))
+                {
+                    return kvp.Value;
+                }
+            }
+
+            return null;
+        }
+
+        private int GetBucketNumber(object key)
+        {
+            return Math.Abs(key.GetHashCode()) % list.Count;
+        }
+    }
+}
---
+++ HashTable/HashTableTests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HashTableTests
+{
+
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        
+        public void ThreeElementsTest()
+        {
+            //Добавление и поиск трёх элементов
+            var hashTest = new HashTable.HashTable(3);
+
+            hashTest.PutPair("One", "Один");
+            hashTest.PutPair("Two", "Два");
+            hashTest.PutPair("Three", "Три");
+
+            Assert.AreEqual(hashTest.GetValueByKey("One"), "Один");
+            Assert.AreEqual(hashTest.GetValueByKey("Two"), "Два");
+            Assert.AreEqual(hashTest.GetValueByKey("Three"), "Три");
+        }
+
+        [TestMethod]
+        public void TwoEquialsElementsTest()
+        {
+            //Добавление одного и того же ключа дважды с разными значениями сохраняет последнее добавленное значение
+            var hashTest = new HashTable.HashTable(2);
+
+            hashTest.PutPair("1", "One");
+            hashTest.PutPair("1", "Three");
+            
+            if (!(hashTest.GetValueByKey("1")).Equals("Three")) throw new Exception();
+        }
+
+        [TestMethod]
+        public void BigElementsTest()
+        {
+            //Поиск одного элемента из тысячи
+            var hashTest = new HashTable.HashTable(10000);
+            var rnd = new Random();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                hashTest.PutPair(i, i + 1);
+            }
+            var key = rnd.Next(0, 9999);
+            Assert.AreEqual(hashTest.GetValueByKey(key), (key+1));
+        }
+
+        [TestMethod]
+        public void BigElementsSearchTests()
+        {
+            //Поиск тысячи недобавленныех ключей
+            var hashTest = new HashTable.HashTable(11000);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                hashTest.PutPair(i, i);
+            }
+
+            for (int i = 10000; i < 11000; i++)
+            {
+                Assert.AreEqual(hashTest.GetValueByKey(i), null);
+            }
+        }
+    }
+}
