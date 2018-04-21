7643c754fe78bac774d0b271a7b533bfbba6cd6b
Пользователь <kazancevaaa33@mail.ru>
2018-04-21 12:15:37 +0500

    SportGames

---
+++ SportGames/SportGames/RequestModel.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportGames
+{
+    /// <summary>
+    /// Информация о заявке на участие в проекте Игра
+    /// </summary>
+    public class RequestModel
+    {
+        /// <summary>
+        /// Дата заполнения 
+        /// </summary> 
+        public DateTime Filled { get; set; }
+
+        /// <summary>
+        /// Вид спорта
+        /// </summary>
+        public Sport SportType { get; set; }
+
+        /// <summary>
+        /// Тип команды
+        /// </summary>
+        public TeamType TeamType { get; set; }
+
+        /// <summary>
+        /// Название команды
+        /// </summary>
+        public string TeamName { get; set; }
+
+        /// <summary>
+        /// Капитан команды
+        /// </summary>
+        public string TeamCaptain { get; set; }
+
+        /// <summary>
+        /// Количество участников
+        /// </summary>
+        public int CountCompetitors { get; set; }
+
+        /// <summary>
+        /// Информация о составе команды
+        /// </summary>
+        public List<TeamMember> TeamMembers { get; set; }
+    }
+
+    /// <summary>
+    /// Участники команды
+    /// </summary>
+    public class TeamMember
+    {
+        /// <summary>
+        /// ФИО участника
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// Пол
+        /// </summary>
+        public Sex Sex { get; set; }
+
+        /// <summary>
+        /// Номер академ.группы игрока
+        /// </summary>
+        public string GroupNumber { get; set; }
+
+
+        /// <summary>
+        /// Колличество полных лет
+        /// </summary>
+        public int Age { get; set; }
+
+        /// <summary>
+        /// Состоит ли игрок в сборной университета по данному виду спорта
+        /// </summary>
+        public bool UniversityTeam { get; set; }
+
+        /// <summary>
+        /// Имеет ли игрок спортивный разряд по данному виду спорта
+        /// </summary>
+        public bool SportsСategory { get; set; }
+
+        /// <summary>
+        /// Контактные данные
+        /// </summary>
+        public ContactDetails Contact { get; set; }
+    }
+
+    /// <summary>
+    /// Контактные данные
+    /// </summary>
+    public class ContactDetails
+    {
+        /// <summary>
+        /// Контактный телефон
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// Адрес электронной почты
+        /// </summary>
+        public string EMail { get; set; }
+    }
+    public enum Sport
+    {
+        Basketball,      // Баскетбол
+        Volleyball,     // Волейбол
+        Handball,      // Гандбол
+        Hockey,       // Хоккей
+    }
+    public enum TeamType
+    {
+        Mens,      // мужская команда
+        Womens,   // женская команда
+        Mixed,   // смешанная команда
+    }
+    public enum Sex
+    {
+        Male,
+        Female,
+    }
+}
+
+
---
+++ SportGames/SportGamesTests/SportGamesTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportGames;
+using System.Collections.Generic;
+
+namespace SportGamesTests
+{
+    [TestClass]
+    public class SportGamesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var dto = new RequestModel
+            {
+                Filled = DateTime.Now,
+                SportType = Sport.Basketball,
+                TeamType = TeamType.Mixed,
+                TeamName = "TheBest",
+                TeamCaptain = "Ivan Ivanov",
+                CountCompetitors = 3,
+                TeamMembers = new List<TeamMember>()
+                {
+                    new TeamMember
+                    {
+                        FullName = "Ivan Ivanov",
+                        Sex = Sex.Male,
+                        GroupNumber="RI-261223",
+                        Age=21,
+                        UniversityTeam=false,
+                        SportsСategory=true,
+                        Contact = new ContactDetails()
+                        {
+                            PhoneNumber="89023494883",
+                            EMail="ivan@mail.ru",
+                        }
+                    },
+                    new TeamMember
+                    {
+                        FullName = "Maria Petrova",
+                        Sex = Sex.Female,
+                        GroupNumber="RI-261221",
+                        Age=20,
+                        UniversityTeam=true,
+                        SportsСategory=true,
+                        Contact = new ContactDetails()
+                        {
+                            PhoneNumber="89023467834",
+                            EMail="maria@yandex.ru",
+
+                        }
+                    },
+                    new TeamMember
+                    {
+                        FullName = "Petr Sidorov",
+                        Sex = Sex.Female,
+                        GroupNumber="RI-261220",
+                        Age=19,
+                        UniversityTeam=false,
+                        SportsСategory=false,
+                        Contact = new ContactDetails()
+                        {
+                            PhoneNumber="89483787834",
+                            EMail="petr@yandex.ru",
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
+
+
+
